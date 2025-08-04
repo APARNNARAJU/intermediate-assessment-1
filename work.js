4d4e816d40d7adb59b9cd7a2e420b807d7164b9d
Aparnna Raju <aparnnaraju3@gmail.com>
2025-08-04 19:55:53 +0530

    Initial commit

---
+++ work.js
@@ -0,0 +1,27 @@
+var x=document.getElementsByClassName("title");
+x[0].addEventListener('mouseover',function f1(){
+    x[0].style.backgroundColor='wheat';
+    x[0].style.fontWeight = 'bold'; 
+ })
+
+var z=document.getElementsByClassName("heading");
+z[0].addEventListener('mouseover',function f1(){
+    z[0].style.backgroundColor='lightskyblue';
+     
+ })
+
+ 
+var y=document.getElementsByClassName("l1");
+for (let i = 0; i < y.length; i++) {
+  y[i].addEventListener('mouseover',function f1(){
+    y[i].style.backgroundColor='aquamarine';
+    y[i].style.fontWeight = 'bold';  
+ })
+}
+var p = document.getElementsByClassName("profile");
+p[0].addEventListener('click', function f1() {
+  p[0].style.transform = 'scale(1.1)'; 
+  p[0].style.backgroundColor='olive';
+  
+});
+
